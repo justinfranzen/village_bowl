fdab39ba0d2caa9f22a3258086d7b74d92e92bf4
Justin Franzen <jtfranz90@gmail.com>
2018-07-24 15:27:27 -0500

    content added to bowling links

--- bowling-links.php
+++ bowling-links.php
@@ -6,9 +6,9 @@
 
 <body>
 
-<div id="league-information-title-img">
+<div id="bowling-links-title-img">
 
-    <h1>LEAGUE INFORMATION</h1>
+    <h1>BOWLING LINKS</h1>
 
 </div>  
     
@@ -16,6 +16,103 @@
     
     <div class="space"></div>
     
+    <div class="row bowl-links">
+    
+        <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+        
+            <img src="images/us-bowling-congress.png" alt="US Bowling Congress" class="bowling-link" />
+            <figcaption>United States Bowling Congress</figcaption>
+            
+        </div>
+        
+        <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+        
+            <img src="images/bowling-proprietors-association-wisconsin.png" alt="Bowling Proprietors Association Wisconsin" class="bowling-link" />
+            <figcaption>Bowling Proprietors Association Wisconsin</figcaption>
+            
+        </div>
+        
+        <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+        
+            <img src="images/wihs-bowling-club.png" alt="WIHS Bowling Club" class="bowling-link" />
+            <figcaption>Wisconsin High School Bowling Club</figcaption>
+            
+        </div>
+        
+        <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+        
+            <img src="images/whsbc-district-1.png" alt="WHSBC District 1" class="bowling-link" />
+            <figcaption>District 1 High School Bowling Club</figcaption>
+            
+        </div>
+    
+    </div>
+    
+    <div class="row bowl-links space">
+    
+        <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+        
+            <img src="images/usbc2.png" alt="United States Bowling Congress" class="bowling-link" />
+            <figcaption>Milwaukee Area USBC</figcaption>
+            
+        </div>
+        
+        <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+        
+            <img src="images/wims-bowling-club.png" alt="WIMS Bowling Club" class="bowling-link" />
+            <figcaption>Wisconsin Middle School Bowling Club</figcaption>
+            
+        </div>
+        
+        <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+        
+            <img src="images/usbc-youth.png" alt="USBC Youth" class="bowling-link" />
+            <figcaption>Wisconsin State USBC Youth Association</figcaption>
+            
+        </div>
+        
+        <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+        
+            <img src="images/smart-scholarship.png" alt="Smart Scholarship" class="bowling-link" />
+            <figcaption>Smart Scholarship</figcaption>
+                
+        </div>
+    
+    
+    </div>
+    
+    <div class="row bowl-links space">
+    
+        <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+        
+            <img src="images/usbc-wi-state.png" alt="WI State Bowling Association" class="bowling-link" />
+            <figcaption>Wisconsin State Bowling Association</figcaption>
+            
+        </div>
+        
+        <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+        
+            <img src="images/bowling-centers-association-of-wisconsin.png" alt="Pepsi USBC Tournament" class="bowling-link" />
+            <figcaption>Pepsi USBC Tournament</figcaption>
+        
+        </div>
+        
+        <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+        
+            <img src="images/wisconsin-pepsi-youth-bowling.png" alt="WI Pepsi Youth Bowling" class="bowling-link" />
+            <figcaption>Wisconsin Pepsi Youth Bowling</figcaption>
+            
+        </div>
+        
+        <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+        
+            <img src="images/bwac-grand-pri-scholarship.png" alt="BWAC Grand Prix Scholarship" class="bowling-link" />
+            <figcaption>BCAW Grand Prix Scholarship Tournament</figcaption>
+            
+        </div>
+    
+    
+    </div>
    
 </div>
     
