88f54793bdf6bf27b474c0543388c4b05309f8c6
Justin Franzen <jtfranz90@gmail.com>
2018-07-24 14:38:55 -0500

    bowling links page commit

---
+++ bowling-links.php
@@ -0,0 +1,29 @@
+<title></title>
+
+<?php
+    include 'includes/header.php';
+?>
+
+<body>
+
+<div id="league-information-title-img">
+
+    <h1>LEAGUE INFORMATION</h1>
+
+</div>  
+    
+<div class="secondary-content">
+    
+    <div class="space"></div>
+    
+   
+</div>
+    
+</body>
+
+<?php
+
+    include 'includes/footer.php';
+
+?>
+    
